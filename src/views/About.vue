3af38eb6b8ff3c75d01e9295b0869bb7a0c7edc3
rayjcr <rayj@qq.com>
2021-11-05 17:02:05 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,66 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>ref && reactive</h1>
+    <h2>
+      ref:{{count}}
+    </h2>
+    <h2>
+      reactive:{{reactiveField}}
+    </h2>
+    <h2>
+      reactive:{{a[0].a}}
+    </h2>
+    <h2>
+      bb:{{b}}
+    </h2>
+
+    <button @click="addRef">addRef</button>
+    <button @click="addReactive">addReactive</button>
+    <button @click="addA">addA</button>
+    <button @click="showbb">showbb</button>
+
   </div>
 </template>
+<script lang="ts">
+import {defineComponent, reactive, readonly, ref, toRefs} from 'vue'
+export default defineComponent({
+  setup(){
+    // console.log(this)
+    const count = ref(0)
+    const state = reactive({
+      reactiveField:0,
+      a:[{a:1}]
+    })
+    const bb = readonly({
+      c:2,
+      b:3
+    })
+    const addRef = () =>{
+      count.value++
+    }
+    const addReactive = () =>{
+      state.reactiveField++
+    }
+    const addA = () =>{
+      state.a[0].a++
+    }
+
+    const showbb = () =>{
+      console.log("5566")
+      bb.b
+    }
+
+    return{
+      count,
+      ...toRefs(state),
+      ...toRefs(bb),
+      addA,
+      addRef,
+      addReactive,
+      showbb
+    }
+
+    // console.log(count,"count")
+  }
+})
+</script>
