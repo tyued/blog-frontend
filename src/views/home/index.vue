98a59b1881a0eace0d25446e6d1bb96914c32bc0
rayjcr <rayj@qq.com>
2021-11-06 21:22:54 +0800

    首页及结构

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,13 @@
 <template>
 <div class="innerPage homepage">
     <div class="fixBanner"></div>
+    <div class="userCard">rrrrr</div>
+    <div class="articleMain">
+        <div class="articletBox">
+            <div class="icon"></div>
+            <div class="articleCont"></div>
+        </div>
+    </div>
 </div>
 </template>
 
@@ -15,16 +22,35 @@ export default class Home extends Vue {}
 <style lang="scss" scoped>
 .homepage{
     font-size: .4rem;
+    padding: 4.5rem .3rem 0 .3rem;
     .fixBanner{
         width: 100%;
         height: 6rem;
         position: fixed;
+        z-index: 1;
         top: 0;
         left: 0;
         border-radius: 0 0 .4rem .4rem;
         background-color: #E2E2E2;
         background: url(../../assets/img/_test.jpeg) center center;
         background-size: 100% auto;
+    }
+    .userCard{
+        width: 100%;
+        height:2.5rem;
+        background: #FFF;
+        border-radius: .2rem;
+        position: relative;
+        z-index: 10;
+        margin: 0 auto;
+        box-shadow: 0 0 .3rem rgba($color: #333333, $alpha: .2);
+    }
+    .articleMain{
+        position: relative;
+        z-index: 10;
+        min-height: 2rem;
+        width: 100%;
+        padding-top: .5rem;
     }
 }
 </style>
